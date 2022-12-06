7f1bc00dd4844449163d2747cc444e9015982544
Linc <1063626686@qq.com>
2022-12-06 15:47:27 +0800

    update pandas concat update regex update def cpu

--- def_get_version.py
+++ def_get_version.py
@@ -6,7 +6,7 @@ import openpyxl
 import glob
 
 
-def hw_get_version(filelist):
+def hw_get_uptime(filelist):
     df_uptime = pd.DataFrame(columns=['设备名', '运行时间'])
     n = 1
     for file_list in filelist:
@@ -17,12 +17,14 @@ def hw_get_version(filelist):
             lines = [i.strip('-') for i in lines]
             data = list(filter(None, lines))
             data_ver = data[0:100]
-            if len(data_ver) > 0:
-                ver_data = data_ver[4]
-                uptime = re.findall(r'[0-9]{1,9}\s[a-zA-Z]{1,5},\s[0-9]{1,9}\s[a-zA-Z]{1,4},\s[0-9]{1,9}\s[a-zA-Z]{1,5},\s[0-9]{1,9}\s[a-zA-Z]{1,7}', ver_data)
-            else:
-                pass
+            for i in range(len(data_ver)):
+                data_ver[i] = re.findall(r'[0-9]{1,9}\s[a-zA-Z]{1,5},\s[0-9]{1,9}\s[a-zA-Z]{1,4},\s[0-9]{1,9}\s[a-zA-Z]{1,5},\s[0-9]{1,9}\s[a-zA-Z]{1,7}', data_ver[i])
+                if len(data_ver[i]) == 0:
+                    pass
+                else:
+                    uptime = data_ver[i]
             dict1 = {'设备名': device, '运行时间': uptime}
+            print(dict1)
             df1 = pd.DataFrame(dict1, index=[n])
             df_uptime = pd.concat([df_uptime, df1], join="outer", axis=0, copy=False, ignore_index=True)
     df_uptime.to_excel('ver.xlsx', sheet_name='hw_version', index=False)
@@ -55,10 +57,38 @@ def hw_get_mem(filelist):
     df_write_mem.close()
 
 
+def hw_get_cpu(filelist):
+    df_cpu = pd.DataFrame(columns=['设备名', 'CPU利用率'])
+    n = 1
+    for file_list in filelist:
+        with open(file_list) as f:
+            device = re.findall(r"\b10.*/", str(f))[0][0:-1]
+            lines = f.readlines()
+            lines = [i.strip() for i in lines]
+            lines = [i.strip('-') for i in lines]
+            data = list(filter(None, lines))
+            data_cpu = data[0:100]
+            for i in range(len(data_cpu)):
+                data_cpu[i] = re.findall(r'CPU utilization.*', data_cpu[i])
+                if len(data_cpu[i]) == 0:
+                    pass
+                else:
+                    cpu_data = data_cpu[i]
+            dict1_cpu = {'设备名': device, 'CPU利用率': cpu_data}
+            df1_cpu = pd.DataFrame(dict1_cpu, index=[n])
+            n = n + 1
+            df_cpu = pd.concat([df_cpu, df1_cpu], join="outer", axis=0, copy=False, ignore_index=True)
+    df_write_cpu = pd.ExcelWriter('ver.xlsx', mode='a', engine='openpyxl', if_sheet_exists='new')
+    df_cpu.to_excel(df_write_cpu, sheet_name='hw_cpu', index=False)
+    df_write_cpu.close()
+
+
 if __name__ == '__main__':
     p = os.path.dirname(os.path.abspath(__file__))
-    p = Path(p + '/东旺/202202生产华为')
+    p = Path(p + '/东旺/')
     verList = list(p.glob("**/display version.txt"))
-    hw_get_version(verList)
+    hw_get_uptime(verList)
     memList = list(p.glob("**/display memory.txt"))
     hw_get_mem(memList)
+    cpuList = list(p.glob("**/display cpu-usage.txt"))
+    hw_get_cpu(cpuList)
