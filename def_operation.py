6c339b5bb5f1ecf2d69a07f254050cc52a738b0e
Linc <1063626686@qq.com>
2022-12-08 12:16:29 +0800

    update pandas concat update regex update def cpu  cisco

--- def_operation.py
+++ def_operation.py
@@ -7,13 +7,15 @@ import openpyxl
 import glob
 
 t = time.localtime()
-t1 =time.strftime('%Y-%m-%d-%H-%M-%S', t)
+t1 = time.strftime('%Y-%m-%d-%H-%M-%S', t)
+
+
 def hw_get_uptime(filelist):
     df_uptime = pd.DataFrame(columns=['设备名', '设备IP地址', '运行时间'])
     n = 1
     for file_list in filelist:
         with open(file_list) as f:
-            device = re.findall(r"[0-9]{1,9}_(.+?)\\", str(f))[0]
+            device = re.findall(r"[0-9]{1,9}_(.+?)/", str(f))[0]
             deviceip = re.findall(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", str(f))[0]
             lines = f.readlines()
             lines = [i.strip() for i in lines]
@@ -29,14 +31,14 @@ def hw_get_uptime(filelist):
             dict1 = {'设备名': device, '设备IP地址': deviceip, '运行时间': uptime}
             df1 = pd.DataFrame(dict1, index=[n])
             df_uptime = pd.concat([df_uptime, df1], join="outer", axis=0, copy=False, ignore_index=True)
-    df_uptime.to_excel('ver-{}.xlsx'.format(t1), sheet_name='hw_version', index=False)
+    df_uptime.to_excel('东旺巡检报告-{}.xlsx'.format(t1), sheet_name='hw_version', index=False)
 
 def hw_get_mem(filelist):
     df_mem = pd.DataFrame(columns=['设备名', '设备IP地址', '内存使用率', 'Total', 'Used'])
     n = 1
     for file_list in filelist:
         with open(file_list) as f:
-            device = re.findall(r"[0-9]{1,9}_(.+?)\\", str(f))[0]
+            device = re.findall(r"[0-9]{1,9}_(.+?)/", str(f))[0]
             deviceip = re.findall(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", str(f))[0]
             lines = f.readlines()
             lines = [i.strip() for i in lines]
@@ -48,7 +50,6 @@ def hw_get_mem(filelist):
                     total_mem = re.findall(r's:(.+?)bytes', data_mem[i])
                     for total in total_mem:
                         total = total
-
                 elif 'Total Memory Used' in data_mem[i]:
                     used_mem = re.findall(r'd Is:(.+?)bytes', data_mem[i])
                     for used in used_mem:
@@ -63,7 +64,7 @@ def hw_get_mem(filelist):
             df1_mem = pd.DataFrame(dict1_mem, index=[n])
             n = n + 1
             df_mem = pd.concat([df_mem, df1_mem], join="outer", axis=0, copy=False, ignore_index=True)
-    df_write_mem = pd.ExcelWriter('ver-{}.xlsx'.format(t1), mode='a', engine='openpyxl', if_sheet_exists='new')
+    df_write_mem = pd.ExcelWriter('东旺巡检报告-{}.xlsx'.format(t1), mode='a', engine='openpyxl', if_sheet_exists='new')
     df_mem.to_excel(df_write_mem, sheet_name='hw_mem', index=False)
     df_write_mem.close()
 
@@ -73,7 +74,7 @@ def hw_get_cpu(filelist):
     n = 1
     for file_list in filelist:
         with open(file_list) as f:
-            device = re.findall(r"[0-9]{1,9}_(.+?)\\", str(f))[0]
+            device = re.findall(r"[0-9]{1,9}_(.+?)/", str(f))[0]
             deviceip = re.findall(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", str(f))[0]
             lines = f.readlines()
             lines = [i.strip() for i in lines]
@@ -90,7 +91,7 @@ def hw_get_cpu(filelist):
             df1_cpu = pd.DataFrame(dict1_cpu, index=[n])
             n = n + 1
             df_cpu = pd.concat([df_cpu, df1_cpu], join="outer", axis=0, copy=False, ignore_index=True)
-    df_write_cpu = pd.ExcelWriter('ver-{}.xlsx'.format(t1), mode='a', engine='openpyxl', if_sheet_exists='new')
+    df_write_cpu = pd.ExcelWriter('东旺巡检报告-{}.xlsx'.format(t1), mode='a', engine='openpyxl', if_sheet_exists='new')
     df_cpu.to_excel(df_write_cpu, sheet_name='hw_cpu', index=False)
     df_write_cpu.close()
 
@@ -100,7 +101,7 @@ def cisco_get_cpu(filelist):
     n = 1
     for file_list in filelist:
         with open(file_list) as f:
-            device = re.findall(r"[0-9]{1,9}_(.+?)\\", str(f))[0]
+            device = re.findall(r"[0-9]{1,9}_(.+?)/", str(f))[0]
             deviceip = re.findall(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", str(f))[0]
             lines = f.readlines()
             lines = [i.strip() for i in lines]
@@ -131,7 +132,7 @@ def cisco_get_cpu(filelist):
                                              ignore_index=True)
                 else:
                     pass
-    df_write_cpu = pd.ExcelWriter('ver-{}.xlsx'.format(t1), mode='a', engine='openpyxl', if_sheet_exists='new')
+    df_write_cpu = pd.ExcelWriter('东旺巡检报告-{}.xlsx'.format(t1), mode='a', engine='openpyxl', if_sheet_exists='new')
     df_cpu_cisco.to_excel(df_write_cpu, sheet_name='cisco_cpu', index=False)
     df_write_cpu.close()
 
@@ -141,7 +142,7 @@ def cisco_get_mem(filelist):
     n = 1
     for file_list in filelist:
         with open(file_list) as f:
-            device = re.findall(r"[0-9]{1,9}_(.+?)\\", str(f))[0]
+            device = re.findall(r"[0-9]{1,9}_(.+?)/", str(f))[0]
             deviceip = re.findall(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", str(f))[0]
             lines = f.readlines()
             lines = [i.strip() for i in lines]
@@ -175,23 +176,7 @@ def cisco_get_mem(filelist):
                     df_mem_cisco = pd.concat([df_mem_cisco, df1_mem], join="outer", axis=0, copy=False, ignore_index=True)
                 else:
                     pass
-
-                # data_mem[i] = re.findall(r'Processor Pool Total.*', data_mem[i])
-    #             if len(data_mem[i]) == 0:
-    #                 pass
-    #             else:
-    #                 mem_data = data_mem[i]
-    #                 total_mem = list(map(int, re.findall(r'Total:(.+?)Used', mem_data[0])))
-    #                 used_mem = list(map(int, re.findall(r'Used:(.+?)Free', mem_data[0])))
-    #                 for total in total_mem:
-    #                     for used in used_mem:
-    #                         mem_utilization = used/total
-    #                 mem_data_cisco = '{:.0f}%'.format(mem_utilization * 100)
-    #                 dict1_mem = {'设备名': device, '设备IP地址': deviceip, '内存利用率': mem_data_cisco, 'Total': total_mem, 'Used': used_mem}
-    #                 df1_mem = pd.DataFrame(dict1_mem, index=[n])
-    #                 n = n + 1
-    #                 df_mem_cisco = pd.concat([df_mem_cisco, df1_mem], join="outer", axis=0, copy=False, ignore_index=True)
-    df_write_mem = pd.ExcelWriter('ver-{}.xlsx'.format(t1), mode='a', engine='openpyxl', if_sheet_exists='new')
+    df_write_mem = pd.ExcelWriter('东旺巡检报告-{}.xlsx'.format(t1), mode='a', engine='openpyxl', if_sheet_exists='new')
     df_mem_cisco.to_excel(df_write_mem, sheet_name='cisco_mem', index=False)
     df_write_mem.close()
 
@@ -201,7 +186,7 @@ def cisco_get_uptime(filelist):
     n = 1
     for file_list in filelist:
         with open(file_list) as f:
-            device = re.findall(r"[0-9]{1,9}_(.+?)\\", str(f))[0]
+            device = re.findall(r"[0-9]{1,9}_(.+?)/", str(f))[0]
             deviceip = re.findall(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", str(f))[0]
             lines = f.readlines()
             lines = [i.strip() for i in lines]
@@ -218,7 +203,7 @@ def cisco_get_uptime(filelist):
                     df1_uptime = pd.DataFrame(dict1_uptime, index=[n])
                     n = n + 1
                     df_uptime_cisco = pd.concat([df_uptime_cisco, df1_uptime], join="outer", axis=0, copy=False, ignore_index=True)
-    df_write_uptime = pd.ExcelWriter('ver-{}.xlsx'.format(t1), mode='a', engine='openpyxl', if_sheet_exists='new')
+    df_write_uptime = pd.ExcelWriter('东旺巡检报告-{}.xlsx'.format(t1), mode='a', engine='openpyxl', if_sheet_exists='new')
     df_uptime_cisco.to_excel(df_write_uptime, sheet_name='cisco_uptime', index=False)
     df_write_uptime.close()
 
